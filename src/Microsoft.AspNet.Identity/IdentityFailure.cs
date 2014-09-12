ba755dbc73144d88dacadf640a47bee1e8906091
Hao Kung <haok@microsoft.com>
2014-09-12 14:15:49 -0700

    stuff

--- src/Microsoft.AspNet.Identity/IdentityFailure.cs
+++ src/Microsoft.AspNet.Identity/IdentityFailure.cs
@@ -3,7 +3,40 @@
 
 namespace Microsoft.AspNet.Identity
 {
-    public enum IdentityFailure
+    public class IdentityFailureDescriber
+    {
+        public virtual string Describe(IdentityFailure failure)
+        {
+            switch (failure)
+            {
+                case IdentityFailure.DuplicateUserName:
+                case IdentityFailure.DuplicateEmail:
+                case IdentityFailure.DuplicateRoleName:
+                case IdentityFailure.UserValidationFailed:
+                case IdentityFailure.UserNameTooShort:
+                case IdentityFailure.RoleNameTooShort:
+                case IdentityFailure.UserNameInvalid:
+                case IdentityFailure.UserAlreadyInRole:
+                case IdentityFailure.UserAlreadyHasPassword:
+                case IdentityFailure.LoginAlreadyAssociated:
+                case IdentityFailure.UserNotInRole:
+                case IdentityFailure.LockoutForUserNotEnabled:
+                case IdentityFailure.RoleValidationFailed:
+                case IdentityFailure.InvalidEmail:
+                case IdentityFailure.InvalidToken:
+                case IdentityFailure.PasswordMismatch:
+                case IdentityFailure.PasswordRequiresDigit:
+                case IdentityFailure.PasswordRequiresLower:
+                case IdentityFailure.PasswordRequiresUpper:
+                case IdentityFailure.PasswordTooShort:
+                case IdentityFailure.PasswordRequiresNonLetterAndDigit:
+                case IdentityFailure.Unknown:
+                default:
+                    return failure.ToString();
+            }
+        }
+
+        public enum IdentityFailure
     {
         DuplicateUserName,
         DuplicateEmail,
