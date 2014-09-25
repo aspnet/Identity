7ff03f738c3b159d8ce6cf3fa676f90f566bef5b
Hao Kung <haok@microsoft.com>
2014-09-25 15:45:57 -0700

    Stuff

--- samples/IdentitySample.Mvc/Startup.cs
+++ samples/IdentitySample.Mvc/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNet.Security.Cookies;
 using Microsoft.AspNet.Security.Facebook;
 using Microsoft.AspNet.Security.Google;
 using Microsoft.AspNet.Security.Twitter;
@@ -12,6 +14,7 @@ using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.DependencyInjection.Fallback;
 using Microsoft.Framework.OptionsModel;
 using IdentitySample.Models;
+using System.Collections.Generic;
 
 namespace IdentitySamples
 {
@@ -43,6 +46,15 @@ namespace IdentitySamples
             //    b.ApplicationServices.GetService<IOptionsAccessor<GoogleAuthenticationOptions>>().Options);
         }
 
+        public static IApplicationBuilder UsePipeline(this IApplicationBuilder builder)
+        {
+            var options = builder.ApplicationServices.GetService<IOptionsAccessor<ApplicationOptions>>().Options;
+            foreach (var middlewareOptions in options.Pipeline)
+            {
+                builder.UseMiddleware(middlewareOptions.Type, middlewareOptions.Args);
+            }
+            return builder;
+        }
 
 
         public static IApplicationBuilder UseGoogleAuthentication(this IApplicationBuilder builder, Func<IApplicationBuilder, GoogleAuthenticationOptions> func)
@@ -91,6 +103,8 @@ namespace IdentitySamples
 
         public void Configure(IApplicationBuilder app)
         {
+            var idOptions = new IdentityOptions();
+
             app.UseServices(services =>
             {
                 // Add EF services to the services container
@@ -147,9 +161,113 @@ namespace IdentitySamples
                     options.ConsumerSecret = "xKUNuKhsRdHD03eLn67xhPAyE1wFFEndFo1X2UJaK2m1jdAxf4";
                 });
 
+                //services.ConfigureApplicationPipeline(app =>
+                //{
+                //    app.UseErrorPage();
+                //    app.UseStaticFiles();
+                //    app.UseIdentity();
+                //    app.UseGoogleAuthentication(options =>
+                //    {
+                //        options.ClientId = "514485782433-fr3ml6sq0imvhi8a7qir0nb46oumtgn9.apps.googleusercontent.com";
+                //        options.ClientSecret = "V2nDD9SkFbvLTqAUBWBBxYAL";
+                //    });
+                //    app.UseFacebookAuthentication(options =>
+                //    {
+                //        options.AppId = "901611409868059";
+                //        options.AppSecret = "4aa3c530297b1dcebc8860334b39668b";
+                //    });
+                //    app.UseTwitterAuthentication(options =>
+                //    {
+                //        options.ConsumerKey = "BSdJJ0CrDuvEhpkchnukXZBUv";
+                //        options.ConsumerSecret = "xKUNuKhsRdHD03eLn67xhPAyE1wFFEndFo1X2UJaK2m1jdAxf4";
+                //    });
+                //    app.UseMvc(routes =>
+                //    {
+                //        routes.MapRoute(
+                //            name: "default",
+                //            template: "{controller}/{action}/{id?}",
+                //            defaults: new { controller = "Home", action = "Index" });
+                //    });
+                //});
+
+                services.SetupOptions<ApplicationOptions>(options =>
+                {
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(ErrorPageMiddleware),
+                        Args = new object[] { ErrorPageOptions.ShowAll, true }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(StaticFileMiddleware),
+                        Args = new object[] { new StaticFileOptions() }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(CookieAuthenticationMiddleware),
+                        Args = new[] {
+                            idOptions.ApplicationCookie
+                        }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(CookieAuthenticationMiddleware),
+                        Args = new[] {
+                            idOptions.ExternalCookie
+                        }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(CookieAuthenticationMiddleware),
+                        Args = new[] {
+                            idOptions.TwoFactorRememberMeCookie
+                        }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(CookieAuthenticationMiddleware),
+                        Args = new[] {
+                            idOptions.TwoFactorUserIdCookie
+                        }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(GoogleAuthenticationMiddleware),
+                        Args = new[] {
+                            new GoogleAuthenticationOptions {
+                                ClientId = "514485782433-fr3ml6sq0imvhi8a7qir0nb46oumtgn9.apps.googleusercontent.com",
+                                ClientSecret = "V2nDD9SkFbvLTqAUBWBBxYAL"
+                            }
+                        }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(FacebookAuthenticationMiddleware),
+                        Args = new[] {
+                            new FacebookAuthenticationOptions {
+                                AppId = "901611409868059",
+                                AppSecret = "4aa3c530297b1dcebc8860334b39668b"
+                            }
+                        }
+                    });
+                    options.Pipeline.Add(new MiddlewareOptions
+                    {
+                        Type = typeof(TwitterAuthenticationMiddleware),
+                        Args = new[] {
+                            new TwitterAuthenticationOptions {
+                                ConsumerKey = "BSdJJ0CrDuvEhpkchnukXZBUv",
+                                ConsumerSecret = "xKUNuKhsRdHD03eLn67xhPAyE1wFFEndFo1X2UJaK2m1jdAxf4"
+                            }
+                        }
+                    });
+                });
+
                 // Add MVC services to the services container
                 services.AddMvc();
             });
+
+            app.SetDefaultSignInAsAuthenticationType(idOptions.DefaultSignInAsAuthenticationType);
+            app.UsePipeline();
 
             /* Error page middleware displays a nice formatted HTML page for any unhandled exceptions in the request pipeline.
              * Note: ErrorPageOptions.ShowAll to be used only at development time. Not recommended for production.
@@ -157,15 +275,15 @@ namespace IdentitySamples
             app.UseErrorPage(ErrorPageOptions.ShowAll);
 
             // Add static files to the request pipeline
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
 
             // Setup identity cookie middleware
             // Add cookie-based authentication to the request pipeline
-            app.UseIdentity();
+            //app.UseIdentity();
 
-            app.UseGoogleAuthentication();
-            app.UseFacebookAuthentication();
-            app.UseTwitterAuthentication();
+            //app.UseGoogleAuthentication();
+            //app.UseFacebookAuthentication();
+            //app.UseTwitterAuthentication();
 
             // Add MVC to the request pipeline
             app.UseMvc(routes =>
@@ -186,5 +304,16 @@ namespace IdentitySamples
             return services.BuildServiceProvider();
         }
 
+    }
+
+    public class MiddlewareOptions
+    {
+        public Type Type { get; set; }
+        public object[] Args { get; set; }
+    }
+
+    public class ApplicationOptions
+    {
+        public IList<MiddlewareOptions> Pipeline { get; } = new List<MiddlewareOptions>();
     }
 }
