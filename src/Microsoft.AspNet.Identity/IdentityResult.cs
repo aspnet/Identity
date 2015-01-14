bc87b975e711bd0fdb4cbdb850541c5a24850648
Suhas Joshi <suhasbjoshi@hotmail.com>
2015-01-14 14:28:50 -0800

    Fixed namespace

--- src/Microsoft.AspNet.Identity/IdentityResult.cs
+++ src/Microsoft.AspNet.Identity/IdentityResult.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Framework.Logging;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Framework.Logging;
 
 namespace Microsoft.AspNet.Identity
 {
