bb20bf10058eae3d272b826b6eb8a86c58e7e3b2
Hao Kung <haok@microsoft.com>
2015-05-08 14:54:15 -0700

    Remove dead code

--- src/Microsoft.AspNet.Identity/Rfc6238AuthenticationService.cs
+++ src/Microsoft.AspNet.Identity/Rfc6238AuthenticationService.cs
@@ -9,21 +9,6 @@ using System.Text;
 
 namespace Microsoft.AspNet.Identity
 {
-    //internal sealed class SecurityToken
-    //{
-    //    private readonly byte[] _data;
-
-    //    public SecurityToken(byte[] data)
-    //    {
-    //        _data = (byte[])data.Clone();
-    //    }
-
-    //    internal byte[] GetDataNoClone()
-    //    {
-    //        return _data;
-    //    }
-    //}
-
     internal static class Rfc6238AuthenticationService
     {
         private static readonly DateTime _unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
