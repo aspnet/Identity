cc3202ba49e9595906ab119cd933f366c618aae9
danroth27 <daroth@microsoft.com>
2015-10-11 00:08:10 -0700

    Add comment to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -292,5 +292,6 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
+# Autoapi ocnfiguration
 autoapi_type = 'dotnet'
 autoapi_dir = '../src'
