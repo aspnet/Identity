9d454acbaa15930d470177909b22e944d83f2e61
Javier Calvarro Nelson <jacalvar@microsoft.com>
2018-01-29 16:54:23 -0800

    Ensure the database exists with the right schema

---
+++ test/Identity.FunctionalTests/Infrastructure/EnsureDatabaseStartupFilter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Identity.FunctionalTests.Infrastructure
+{
+    public class EnsureDatabaseStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return appBuilder =>
+            {
+                EnsureDatabase(appBuilder);
+                next(appBuilder);
+            };
+        }
+
+        private void EnsureDatabase(IApplicationBuilder appBuilder)
+        {
+            var scopeFactory = appBuilder.ApplicationServices.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<IdentityDbContext>();
+                context.Database.Migrate();
+            }
+        }
+    }
+}
--- test/Identity.FunctionalTests/Infrastructure/ServerFactory.cs
+++ test/Identity.FunctionalTests/Infrastructure/ServerFactory.cs
@@ -6,31 +6,21 @@ using System.IO;
 using System.Net.Http;
 using Identity.DefaultUI.WebSite;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.AspNetCore.Identity.FunctionalTests.Infrastructure
 {
-    class ServerFactory
+    public class ServerFactory
     {
-        internal static HttpClient CreateDefaultClient()
+        public static HttpClient CreateDefaultClient() =>
+            CreateDefaultClient(CreateDefaultServer());
+
+        public static TestServer CreateDefaultServer()
         {
             var builder = WebHostBuilderFactory
                 .CreateFromTypesAssemblyEntryPoint<Startup>(new string[] { })
-                .UseSolutionRelativeContentRoot(Path.Combine("test", "WebSites", "Identity.DefaultUI.WebSite"));
-
-            var server = new TestServer(builder);
-            var client = new HttpClient(new CookieContainerHandler(server.CreateHandler()))
-            {
-                BaseAddress = new Uri("https://localhost")
-            };
-
-            return client;
-        }
-
-        internal static TestServer CreateDefaultServer()
-        {
-            var builder = WebHostBuilderFactory
-                .CreateFromTypesAssemblyEntryPoint<Startup>(new string[] { })
-                .UseSolutionRelativeContentRoot(Path.Combine("test", "WebSites", "Identity.DefaultUI.WebSite"));
+                .UseSolutionRelativeContentRoot(Path.Combine("test", "WebSites", "Identity.DefaultUI.WebSite"))
+                .ConfigureServices(sc => sc.AddSingleton<EnsureDatabaseStartupFilter>());
 
             var server = new TestServer(builder);
             return server;
