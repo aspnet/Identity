4ae7a26c56bb32fab3bd3463ed17e51eec75f025
Hao Kung <haok@microsoft.com>
2018-02-05 14:23:45 -0800

    Cleanup

--- test/Identity.Test/SecurityStampValidatorTest.cs
+++ test/Identity.Test/SecurityStampValidatorTest.cs
@@ -71,43 +71,6 @@ namespace Microsoft.AspNetCore.Identity.Test
         [InlineData(false)]
         public async Task OnValidatePrincipalTestSuccess(bool isPersistent)
         {
-            //var user = new TestUser("test");
-            //var userManager = MockHelpers.MockUserManager<TestUser>();
-            //var claimsManager = new Mock<IUserClaimsPrincipalFactory<TestUser>>();
-            //var identityOptions = new Mock<IOptions<IdentityOptions>>();
-            //identityOptions.Setup(a => a.Value).Returns(new IdentityOptions());
-            //var options = new Mock<IOptions<SecurityStampValidatorOptions>>();
-            //options.Setup(a => a.Value).Returns(new SecurityStampValidatorOptions { ValidationInterval = TimeSpan.Zero });
-            //var httpContext = new Mock<HttpContext>();
-            //var contextAccessor = new Mock<IHttpContextAccessor>();
-            //contextAccessor.Setup(a => a.HttpContext).Returns(httpContext.Object);
-            //var id = new ClaimsIdentity(IdentityConstants.ApplicationScheme);
-            //id.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
-            //var principal = new ClaimsPrincipal(id);
-
-            //var signInManager = new Mock<SignInManager<TestUser>>(userManager.Object,
-            //    contextAccessor.Object, claimsManager.Object, identityOptions.Object, null, new Mock<IAuthenticationSchemeProvider>().Object);
-            //signInManager.Setup(s => s.ValidateSecurityStampAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user).Verifiable();
-            //signInManager.Setup(s => s.CreateUserPrincipalAsync(user)).ReturnsAsync(principal).Verifiable();
-            //var services = new ServiceCollection();
-            //services.AddSingleton(options.Object);
-            //services.AddSingleton(signInManager.Object);
-            //services.AddSingleton<ISecurityStampValidator>(new SecurityStampValidator<TestUser>(options.Object, signInManager.Object, new SystemClock()));
-            //httpContext.Setup(c => c.RequestServices).Returns(services.BuildServiceProvider());
-
-            //var properties = new AuthenticationProperties { IssuedUtc = DateTimeOffset.UtcNow.AddSeconds(-1), IsPersistent = isPersistent };
-            //var ticket = new AuthenticationTicket(principal,
-            //    properties,
-            //    IdentityConstants.ApplicationScheme);
-            //var context = new CookieValidatePrincipalContext(httpContext.Object, new AuthenticationSchemeBuilder(IdentityConstants.ApplicationScheme) { HandlerType = typeof(NoopHandler) }.Build(), new CookieAuthenticationOptions(), ticket);
-            //Assert.NotNull(context.Properties);
-            //Assert.NotNull(context.Options);
-            //Assert.NotNull(context.Principal);
-            //await
-            //    SecurityStampValidator.ValidatePrincipalAsync(context);
-            //Assert.NotNull(context.Principal);
-            //signInManager.VerifyAll();
-
             var user = new TestUser("test");
             var httpContext = new Mock<HttpContext>();
 
