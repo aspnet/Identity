b19f9b021ccff63f6b19a272a938c3cc3093bd91
Ryan Brandenburg <rybrande@microsoft.com>
2018-09-19 16:50:52 -0700

    Don't lie

--- samples/IdentitySample.DefaultUI/Startup.cs
+++ samples/IdentitySample.DefaultUI/Startup.cs
@@ -35,7 +35,6 @@ namespace IdentitySample.DefaultUI
             services.AddMvc();
 
             services.AddDefaultIdentity<ApplicationUser>()
-                 .AddDefaultUI(Microsoft.AspNetCore.Identity.UI.UIFramework.Bootstrap4)
                  .AddRoles<IdentityRole>()
                  .AddEntityFrameworkStores<ApplicationDbContext>();
         }
